559603fc02db9305df557a42e2d7477f3d9da08d
milagros <milagroscalcina.minos@gmail.com>
2016-01-14 12:14:19 -0500

    arreglando

--- assets/js/main.js
+++ assets/js/main.js
@@ -117,3 +117,19 @@
 	});
 
 })(jQuery);
+
+
+var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        coverflow: {
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows : true
+        }
+    });
